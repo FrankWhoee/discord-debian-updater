fc0f0a6a69250627f08b68dd5fd1bc39392a9819
FrankWhoee <huifrank@live.com>
2024-06-25 23:34:16 -0700

    Initial commit

---
+++ updatediscord.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version=$(ls ~/.config/discord | sed -n "s/^.*\([0-9]\+.[0-9]\+.[0-9]\+\).*$/\1/p")
+
+echo "Current version: $version"
+new_version=$(echo $version | awk -F. '{$NF = $NF + 1; print $0}' OFS=.)
+echo "Downloading $new_version"
+wget https://dl.discordapp.net/apps/linux/$new_version/discord-$new_version.deb
+sudo apt-get install ./discord-$new_version.deb
+rm discord-$new_version.deb
